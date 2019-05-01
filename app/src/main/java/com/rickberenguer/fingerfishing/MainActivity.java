e34c0a17078acd8305a9543d1eba3938576a1a2b
Erick Hobbs <37117731+ERICK-HOBBS@users.noreply.github.com>
2019-05-01 19:18:44 -0400

    commit 1
    mainActivity switching to hiscoreActivity

--- app/src/main/java/com/rickberenguer/fingerfishing/MainActivity.java
+++ app/src/main/java/com/rickberenguer/fingerfishing/MainActivity.java
@@ -1,13 +1,44 @@
 package com.rickberenguer.fingerfishing;
 
+
+import android.content.Intent;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button hiScoreButton;
+
+    int screenWidth;
+    int screenHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //find out the width and height of the screen
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        screenWidth = size.x;
+        screenHeight = size.y;
+
+        hiScoreButton = (Button) findViewById(R.id.hiScoreButton);
+        hiScoreButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openHiScoreActivity();
+            }
+        });
+    }
+
+    public void openHiScoreActivity(){
+        Intent intent = new Intent(this, HiScoreActivity.class);
+        startActivity(intent);
     }
 }
