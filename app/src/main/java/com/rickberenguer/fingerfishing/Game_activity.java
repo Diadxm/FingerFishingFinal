9bee7571a3751b3598564547a8278821e1a2a3d2
Erick Hobbs <37117731+ERICK-HOBBS@users.noreply.github.com>
2019-05-06 20:59:04 -0400

    commit 10
    fishing pole image starts roate animation on cast swipe button

--- app/src/main/java/com/rickberenguer/fingerfishing/Game_activity.java
+++ app/src/main/java/com/rickberenguer/fingerfishing/Game_activity.java
@@ -3,11 +3,17 @@ package com.rickberenguer.fingerfishing;
 import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Build;
 import android.view.Display;
 import android.widget.ProgressBar;
 import android.widget.SeekBar;
 import android.widget.TextView;
-
+import android.view.View;
+import android.widget.ImageView;
+import android.support.annotation.RequiresApi;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import com.ebanx.swipebtn.OnStateChangeListener;
 import com.ebanx.swipebtn.SwipeButton;
 
@@ -25,6 +31,8 @@ public class Game_activity extends AppCompatActivity {
 
     int screenWidth;
     int screenHeight;
+
+    ImageView fishingPoleImage;
     //////////////////////
     /////////////////////
 
@@ -36,9 +44,9 @@ public class Game_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_activity);
 
-
         //Erick-Hobbs
         //////////////
+        fishingPoleImage = findViewById(R.id.fishingPole);
         //find out the width and height of the screen
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
@@ -50,7 +58,10 @@ public class Game_activity extends AppCompatActivity {
         castSwipe.setOnStateChangeListener(new OnStateChangeListener() {
             @Override
             public void onStateChange(boolean active) {
-                
+                RotateAnimation rotate = new RotateAnimation(0, 180, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setInterpolator(new LinearInterpolator());
+                fishingPoleImage.startAnimation(rotate);
             }
         });
         //////////////////////
